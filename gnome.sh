1eb89c3b2c9c48e49c9446ab0c03b6ee8a82ab0c
Marian Hahne <marian@marianhahne.de>
2024-09-07 22:03:58 +0200

    feat: add script for Gnome config

---
+++ gnome.sh
@@ -0,0 +1,21 @@
+echo "Config Gnome..."
+
+# Gives you previews in the file manager when pressing space
+sudo dnf install -y sushi
+
+sudo dnf install -y gnome-tweaks
+
+# Center new windows in the middle of the screen
+gsettings set org.gnome.mutter center-new-windows true
+
+# Set Cascadia Mono as the default monospace font
+#gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Nerd Font 10'
+
+# Reveal week numbers in the Gnome calendar
+gsettings set org.gnome.desktop.calendar show-weekdate true
+
+gsettings set org.gnome.desktop.screensaver lock-enabled true
+gsettings set org.gnome.desktop.session idle-delay 600
+
+# Fractional Scaling
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 echo "Setup Fedora client"
 
 ./programs.sh
+./gnome.sh
