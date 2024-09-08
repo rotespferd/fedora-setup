6f2b6e3e7146606f2f7e708a0ab7d5def6e28ed4
Marian Hahne <marian@marianhahne.de>
2024-09-08 12:41:16 +0200

    feat: install user programs

--- programs.sh
+++ programs.sh
@@ -3,5 +3,8 @@ echo "Install programs..."
 # Install basics
 sudo dnf install -y wget curl git zsh
 
-# Install edditors
+# Install editors
 sudo dnf install -y neovim vim
+
+# Install user programs
+sudo dnf install -y nextcloud-client nextcloud-client-nautilus keepassxc
