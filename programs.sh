d60f872f07016bc0d6cc09ed8c1ed052c3bb5e57
Marian Hahne <marian@marianhahne.de>
2024-09-08 15:34:10 +0200

    feat: install some more programs

--- programs.sh
+++ programs.sh
@@ -6,5 +6,12 @@ sudo dnf install -y wget curl git zsh
 # Install editors
 sudo dnf install -y neovim vim
 
+# Install cli utilities
+sudo dnf install -y alacritty kitty eza bat htop btop screenfetch neofetch
+
+# Install zellij
+sudo dnf copr enable -y varlad/zellij 
+sudo dnf install -y zellij
+
 # Install user programs
 sudo dnf install -y nextcloud-client nextcloud-client-nautilus keepassxc
