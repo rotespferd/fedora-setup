5b3746de7d084e164dc694fb222eaef6ff0a651d
Marian Hahne <marian@marianhahne.de>
2024-09-09 10:46:42 +0200

    feat: add Signal to flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -6,3 +6,4 @@ flatpak install -y flathub de.schmidhuberj.DieBahn
 flatpak install -y flathub dev.geopjr.Tuba
 flatpak install -y flathub us.zoom.Zoom
 flatpak install -y flathub com.todoist.Todoist
+flatpak install -y flathub org.signal.Signal
