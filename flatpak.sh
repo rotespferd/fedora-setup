abc3ca8f0af0a54f77766c3a6dad88d6e776553d
Marian Hahne <marian@marianhahne.de>
2024-09-09 09:24:59 +0200

    feat: add Zoom and Todoist to flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -4,3 +4,5 @@ flatpak install -y flathub org.gnome.Extensions
 flatpak install -y flathub com.mattjakeman.ExtensionManager
 flatpak install -y flathub de.schmidhuberj.DieBahn
 flatpak install -y flathub dev.geopjr.Tuba
+flatpak install -y flathub us.zoom.Zoom
+flatpak install -y flathub com.todoist.Todoist
