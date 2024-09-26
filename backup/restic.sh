6c19f6cd40a34219801a3f1963a5301557d6b4a5
Marian Hahne <marian@marianhahne.de>
2024-09-26 20:56:59 +0200

    feat: Add restic  forget services to NAS

--- backup/restic.sh
+++ backup/restic.sh
@@ -27,6 +27,10 @@ cat >$HOME/.config/restic_excluded_files.txt <<EOF
 /home/${USER}/.local/share/waydroid
 EOF
 
+
+#
+# restic backup service
+#
 cat >/tmp/restic_backup_nas.service <<EOF
 [Unit]
 Description=Restic backup service to NAS
@@ -59,3 +63,46 @@ sudo mv /tmp/restic_backup_nas.timer /etc/systemd/user/restic_backup_nas.timer
 systemctl --user daemon-reload
 
 systemctl --user enable --now restic_backup_nas.timer
+
+#
+# restic forget service
+#
+cat >/tmp/restic_forget_nas.service <<EOF
+[Unit]
+Description=Restic forget service to NAS
+
+[Service]
+Type=simple
+ExecStart=/bin/bash -c 'export RESTIC_PASSWORD=${repopw}; restic -r ${repopath} forget --host ${my_hostname} --tag home --tag ${USER} --prune --keep-hourly 10 --keep-daily 10 --keep-weekly 5 --keep-monthly 12 --keep-yearly 10; unset RESTIC_PASSWORD'
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo mv /tmp/restic_forget_nas.service /etc/systemd/user/restic_forget_nas.service
+systemctl --user daemon-reload
+
+# Create restic forget timer to run forget every day
+cat >/tmp/restic_forget_nas.timer <<EOF
+[Unit]
+Description=Restic forget timer to NAS
+
+[Timer]
+OnCalendar=*-*-* 18:00:00
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+
+sudo mv /tmp/restic_forget_nas.timer /etc/systemd/user/restic_forget_nas.timer
+systemctl --user daemon-reload
+
+systemctl --user enable --now restic_forget_nas.timer
+
+echo "Restic backup and forget service configured"
+echo "Restic backup and forget service will run every 3 hours and every day at 18:00"
+echo "Restic backup will keep 7 daily, 4 weekly and 6 monthly snapshots"
+echo "Restic backup will exclude the following files:"
+cat $HOME/.config/restic_excluded_files.txt
+echo "Restic backup and forget service can be controlled with systemctl --user start/stop/status restic_backup_nas.service and restic_forget_nas.service"
