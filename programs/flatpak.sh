1448a05ffd3dd9dc062b67a92d14d3108b21ca7f
Marian Hahne <marian@marianhahne.de>
2024-09-27 20:24:17 +0200

    feat: Add Anytype installation to flatpak script

--- programs/flatpak.sh
+++ programs/flatpak.sh
@@ -7,3 +7,4 @@ flatpak install -y flathub dev.geopjr.Tuba
 flatpak install -y flathub us.zoom.Zoom
 flatpak install -y flathub com.todoist.Todoist
 flatpak install -y flathub org.signal.Signal
+flatpak install -y flathub io.anytype.anytype
