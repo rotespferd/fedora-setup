73f31cbae4205b78bc6b535e10aa85d88c282f48
Marian Hahne <marian@marianhahne.de>
2024-09-30 07:25:41 +0200

    refactor: Update tex.sh to include additional texlive packages

--- programs/tex.sh
+++ programs/tex.sh
@@ -4,7 +4,7 @@ echo "Install LaTeX and some packages..."
 sudo dnf install -y texlive
 
 # Install additional texlive packages
-sudo dnf install -y texlive-moderncv texlive-fontawesome5 texlive-multirow texlive-arydshln lastpage
+sudo dnf install -y texlive-moderncv texlive-fontawesome5 texlive-multirow texlive-arydshln lastpage texlive-luatex texlive-fontspec
 
 # Install texstudio for easy editing
 sudo dnf install -y texstudio
